35ab82c8d17a2ca0ec78802cafdd6173221ae83f
Maksim <maksim_belotelov@mail.ru>
2022-12-15 00:56:28 +0300

    add Ex41

---
+++ Ex41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Пользователь вводит с клавиатуры М чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+
+Console.Write("Введите количество чисел (M): ");
+int m=Convert.ToInt32(Console.ReadLine());
+
+int[] nums = InpArr(m);
+
+Console.WriteLine($"Количество положительных элементов: {CountPosNum(nums)}.");
+
+int[] InpArr(int size)
+{
+    int[] resArr=new int [size];
+
+    for(int i=0; i<size; i++)
+    {
+        Console.Write($"Введите число {i+1}: ");
+        resArr[i]=Convert.ToInt32(Console.ReadLine());
+    }
+    return resArr;
+}
+
+int CountPosNum(int[] nums)
+{
+    int result=0;
+    
+    for(int i=0; i<nums.Length; i++)
+        if(nums[i]>0)
+            result++;
+
+    return result;
+}
